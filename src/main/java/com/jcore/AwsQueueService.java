4edcc18886da7f3709e013bf0794a46ec5b79d68
Sebastiaan van der Made <made@fd.nl>
2025-08-29 13:51:38 +0200

    Working Application + Queue!

--- src/main/java/com/jcore/AwsQueueService.java
+++ src/main/java/com/jcore/AwsQueueService.java
@@ -2,9 +2,13 @@ package com.jcore;
 
 import software.amazon.awscdk.services.sns.CfnTopic;
 import software.amazon.awscdk.services.sqs.CfnQueue;
+import software.amazon.awscdk.services.sqs.CfnQueuePolicy;
 import software.constructs.Construct;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AwsQueueService {
 
@@ -29,10 +33,38 @@ public class AwsQueueService {
     }
 
     public CfnQueue createQueue(String queueName) {
-        return CfnQueue.Builder
+        var queue = CfnQueue.Builder
                 .create(scope, prefix + "sqs-queue")
                 .queueName(queueName)
                 .build();
+
+        var policy = CfnQueuePolicy.Builder
+                .create(scope, prefix + "sqs-queue-access-policy")
+                .queues(List.of(queue.getAttrQueueUrl()))
+                .policyDocument(createPolicyDocument(queue.getAttrArn()))
+                .build();
+        return queue;
+    }
+
+    private Map<String, Object> createPolicyDocument(String queueArn) {
+        var principal = new HashMap<String, Object>();
+        principal.put("Service", "sns.amazonaws.com");
+
+        var condition = new HashMap<String, Object>();
+        condition.put("ArnLike", Map.of("aws:SourceArn", "arn:aws:sns:*:*:*"));
+
+        var statement = new HashMap<String, Object>();
+        statement.put("Effect", "Allow");
+        statement.put("Principal", principal);
+        statement.put("Action", "sqs:SendMessage");
+        statement.put("Resource", queueArn);
+        statement.put("Condition", condition);
+
+        var policyDocument = new HashMap<String, Object>();
+        policyDocument.put("Version", "2012-10-17");
+        policyDocument.put("Statement", Collections.singletonList(statement));
+
+        return policyDocument;
     }
 
 
